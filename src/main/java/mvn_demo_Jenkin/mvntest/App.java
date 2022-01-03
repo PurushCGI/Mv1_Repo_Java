6309b7b3a4f135612058defbea3d8637a3892c52
purushotham <purushotham.reddy@cgi.com>
2022-01-03 17:10:24 +0530

    First Commit

---
+++ src/main/java/mvn_demo_Jenkin/mvntest/App.java
@@ -0,0 +1,13 @@
+package mvn_demo_Jenkin.mvntest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
